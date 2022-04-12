7f05bebfade6bb1598635432b1ba3119a3ac7ef9
mustafa <98843909+born53@users.noreply.github.com>
2022-04-13 02:45:25 +0300

    Update denemGit.java

--- src/denemGit.java
+++ src/denemGit.java
@@ -1,7 +1,9 @@
 public class denemGit {
     public static void main(String[] args) {
-        String str ="yeni commit";
+        String str ="yeni commit simdi intelijiye git";
         System.out.println(str);
+      
+            
     }
 
 }
